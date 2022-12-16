9112bb37fadfc73264ff10d8cac50882da5050f9
drowrin <drowrin@gmail.com>
2022-12-16 18:31:39 -0500

    remove hanging references

--- modron/db/__init__.py
+++ modron/db/__init__.py
@@ -1,4 +1,1 @@
 from modron.db.conn import DBConn as DBConn
-from modron.db.models import Character as Character
-from modron.db.models import Game as Game
-from modron.db.models import Player as Player
