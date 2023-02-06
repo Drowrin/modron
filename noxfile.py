ddaab6f53d5f36362cbcf32a3a7083e2bb37253a
drowrin <drowrin@gmail.com>
2023-02-06 15:57:50 -0500

    split requirements files and remove poetry

--- noxfile.py
+++ noxfile.py
@@ -1,20 +1,27 @@
 import nox
 
+
 @nox.session
 def lint(session: nox.Session):
-    session.install("poetry")
-    session.run("poetry", "install")
-    
+    session.install("-r", "requirements-dev.txt")
+
     session.run("black", "--check", "modron")
+    session.run("isort", "-c", "modron")
     session.run("codespell", "modron")
-    session.run("mypy", "modron")
+
+
+@nox.session
+def typecheck(session: nox.Session):
+    session.install("-r", "requirements.txt")
+    session.install("-r", "requirements-types.txt")
     
+    session.run("pyright", "modron")
+
 
 @nox.session
 def fixes(session: nox.Session):
-    session.install("poetry")
-    session.run("poetry", "install")
-    
-    session.run("isort", "modron")
+    session.install("-r", "requirements-dev.txt")
+
     session.run("black", "modron")
+    session.run("isort", "modron")
     session.run("codespell", "modron", "-i", "2")
