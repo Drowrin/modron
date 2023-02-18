f810032b607c4510810848a05f61b0249144ac73
drowrin <drowrin@gmail.com>
2023-02-18 15:58:14 -0500

    remove debug sql lines

--- modron/db/conn.py
+++ modron/db/conn.py
@@ -6,13 +6,6 @@ import asyncpg
 async def connect(url: str) -> asyncpg.Connection[asyncpg.Record]:
     conn = await asyncpg.connect(url)
 
-    await conn.execute(
-        "DROP TABLE IF EXISTS Players;"
-        "DROP TABLE IF EXISTS Characters;"
-        "DROP TABLE IF EXISTS Games;"
-        "DROP TYPE IF EXISTS game_status;"
-    )
-
     with open("modron/db/schema.sql") as f:
         await conn.execute(f.read())
 
